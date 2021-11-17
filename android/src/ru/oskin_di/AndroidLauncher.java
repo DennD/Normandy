0057bacf9d31f204da885557cb5364b1b1b4dd4d
OskinDI <denis_oskin@rambler.ru>
2021-11-17 22:08:50 +0300

    Initial commit

---
+++ android/src/ru/oskin_di/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.oskin_di;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.oskin_di.NormandyApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NormandyApp(), config);
+	}
+}
---
+++ desktop/src/ru/oskin_di/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.oskin_di.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.oskin_di.NormandyApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NormandyApp(), config);
+	}
+}
