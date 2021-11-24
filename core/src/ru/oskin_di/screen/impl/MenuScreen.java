c7386a361a0bf957b486ce47584e1ff0771c663f
OskinDI <denis_oskin@rambler.ru>
2021-11-25 00:21:16 +0300

    Add move ship in new Coordinate system.

--- core/src/ru/oskin_di/screen/impl/MenuScreen.java
+++ core/src/ru/oskin_di/screen/impl/MenuScreen.java
@@ -5,14 +5,19 @@ import com.badlogic.gdx.math.Vector2;
 import ru.oskin_di.math.Rect;
 import ru.oskin_di.screen.BaseScreen;
 import ru.oskin_di.sprite.impl.Background;
+import ru.oskin_di.sprite.impl.Ship;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Texture bg;
     private Vector2 pos;
+    private Vector2 v;
 
     private Background background;
+    private Ship ship;
+
+    private final float V_LEN = 0.01f;
 
     @Override
     public void show() {
@@ -20,13 +25,17 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("normandy.png");
         bg = new Texture("galactic.jpg");
         pos = new Vector2();
+        v = new Vector2();
         background = new Background(bg);
+        ship = new Ship(img);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        ship.resize(worldBounds);
+
     }
 
     @Override
@@ -34,7 +43,19 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         batch.begin();
         background.draw(batch);
+        ship.draw(batch);
+
+        pos.set(ship.getLeft() + ship.getHalfWidth(), ship.getBottom() + ship.getHalfHeight());
+
+
+        if (pos.dst(getTouch()) > V_LEN) {
+            ship.move(v);
+        } else {
+            ship.stop(getTouch());
+        }
+
         batch.end();
+
     }
 
     @Override
@@ -46,7 +67,7 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
+        v.set(touch.cpy().sub(pos).setLength(V_LEN));
         return super.touchDown(touch, pointer, button);
     }
 }
